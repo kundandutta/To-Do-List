da01ed1d07de294f39febe1ea887a6bd696b240a
kundan kumar dutta <duttakundan921@gmail.com>
2023-02-04 19:28:09 +0530

    changes regarding list items

--- list.js
+++ list.js
@@ -1,7 +1,15 @@
-function addData(){
+function addData(list){
 
     var data =document.getElementById("box").value;
-    var listArr = ['']
+    var rules = ['']
     document.getElementById("task1")
-
+    for(var i=0; i<data.length; i++){
+        rules += data[i].value;
+    }
+    var li = document.createElement("li");
+    var rule = document.createTextNode(rules);
+    li.appendChild(rule);
+    document.getElementById("list").appendChild(li);
 }
+    
+        //if(data=== ""){alert("Please enter something" )}
